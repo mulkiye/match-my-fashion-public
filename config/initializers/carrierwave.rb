2b5062720b1ffa1601b97d0d95cbc191be4f0981
mulkiye <mulkiye@tamu.edu>
2020-11-08 04:36:37 -0600

    carrier
    carries image

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,16 @@
 CarrierWave.configure do |config|
+    config.ignore_integrity_errors = false
+    config.ignore_processing_errors = false
+    config.ignore_download_errors = false
+    
   config.fog_credentials = {
-    provider:              'AWS',                            # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
-    region:                'us-east-1'
+    provider:              'AWS',
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"]
   }
-  config.fog_directory = ENV["AWS_BUCKET"]                   # required
+  config.fog_directory = ENV["AWS_BUCKET"]
   config.fog_public    = false
-  config.cache_dir.    = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on heroku
+  config.cache_dir   = "#{Rails.root}/tmp/uploads"
 
   # store files locally in test and development environments
   if Rails.env.test? || Rails.env.development?
@@ -18,26 +21,3 @@ CarrierWave.configure do |config|
     config.storage = :fog
   end
 end
-
-#CarrierWave.configure do |config|
-#  config.ignore_integrity_errors = false
-#  config.ignore_processing_errors = false
-#  config.ignore_download_errors = false
-#
-#  if ENV.has_key? 'S3_KEY' and ENV.has_key? 'S3_SECRET'
-#    puts "Image Host: AWS"
-#    config.fog_provider = 'fog/aws'
-#    config.fog_credentials = {
-#      provider: 'AWS',
-#      aws_access_key_id: ENV['S3_KEY'],
-#      aws_secret_access_key: ENV['S3_SECRET'],
-#    }
-#    config.fog_directory = 'fashionnxt-cc'
-#    config.storage = :fog
-#  else
-#    puts "Image Host: local filesystem"
-#    config.storage = :file
-#  end
-#
-#  config.cache_dir = "#{Rails.root}/tmp/uploads"
-#end
